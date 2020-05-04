6caacf7217d300ebd2961eed9d6cd589718cb62c
jckr <jckr.github@gmail.com>
2020-05-03 22:59:48 -0700

    updates

---
+++ gatsby/src/components/footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+  Box,
+  Flex,
+  Link,
+} from 'rebass'
+
+export default props =>
+  <Box
+    as='footer'
+    py={5}
+    color='background'
+    bg='text'
+  >
+    <Box
+      sx={{
+        maxWidth: 'wide',
+        mx: 'auto',
+        px: 3,
+      }}>
+      <Link href='/' variant='nav'>React-Sim</Link>
+      <Link href='/about' variant='nav'>Docs</Link>
+      <Link href='https://github.com/jckr/react-sim' variant='nav'>GitHub</Link>
+    </Box>
+  </Box>
