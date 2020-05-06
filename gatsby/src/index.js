26b4c45b1fc68b13890db7c69254d93ce12f17fb
jckr <jckr.github@gmail.com>
2020-05-05 18:41:33 -0700

    upgrade docs

--- gatsby/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Layout from './components/layout';
-export { default as Layout } from './components/layout';
-
-export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>;
-};
