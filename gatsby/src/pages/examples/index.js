26053d1536d4d3023dc6e8b0614d66d04ff7a6d3
jckr <jckr.github@gmail.com>
2020-06-10 19:37:57 -0700

    rough example page with thumbnails and gifs

--- gatsby/src/pages/examples/index.js
+++ gatsby/src/pages/examples/index.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import { Flex, Text } from 'rebass';
+import React, { useState } from 'react';
+import { Flex, Link, Text } from 'rebass';
+import Image, { camel } from '../../components/image';
+import examples, { gifs } from '../../components/examples/';
 
-import examples from '../../components/examples/';
-
-const ExampleCard = ({ name, title }) => (
-  <Flex sx={{ height: 332, width: 332, mr: 2, mb: 2, position: 'relative' }}>
-    <Text sx={{ position: 'absolute', bottom: 2, left: 2 }}>{title}</Text>
-  </Flex>
-);
+const ExampleCard = ({ name, title }) => {
+  const [hover, setHover] = useState(false);
+  return (
+    <Flex
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
+      sx={{
+        width: 342,
+        p: 1,
+        border: '1px solid black',
+        m: 1,
+        position: 'relative',
+      }}
+    >
+      <Link href={`examples/${name}`}>
+        {hover ? (
+          <img src={gifs[name]} alt={name} />
+        ) : (
+          <Image name={name} fixed />
+        )}
+        <Text
+          sx={{
+            fontSize: 4,
+            fontWeight: 'bold',
+          }}
+        >
+          {title}
+        </Text>
+      </Link>
+    </Flex>
+  );
+};
 
 const Examples = props => {
   return (
